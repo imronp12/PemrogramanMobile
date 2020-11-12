339e6f24f3c338231547cfcce52f21ce11d7fada
imronp12 <54977339+imronp12@users.noreply.github.com>
2020-11-12 23:18:24 +0700

    Add files via upload

---
+++ lib/screens/profile.dart
@@ -0,0 +1,46 @@
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+
+// import '../constanst.dart';
+
+
+
+// class Profilepage extends StatefulWidget {
+//   static const routename= "/profile";
+//   @override
+//   _ProfilepageState createState() => _ProfilepageState();
+// }
+
+// class _ProfilepageState extends State<Profilepage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         color: ColorPalette.primaryColor,
+//         padding: EdgeInsets.all(20.0),
+//         child: ListView(
+//           children: <Widget>[
+//             Center(
+//               child: Column(
+//                 children: <Widget>[
+//                   _iconProfile(),
+//                   // _titleDescription(),
+//                   // _textField(),
+//                   // _buildButton(context),
+//                 ],
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+//   Widget _iconProfile(){
+//     return Image.asset(
+//       "assets/images/avatar.jpg",
+//         width: 250,
+//         height: 250,
+//     );
+//   }
